c38af32fa9ffc510c3167c87a80f795acd7b76f4
Dragos Strugar <itsdragos@gmail.com>
2020-03-13 14:30:22 +0300

    feat: add activeprocess model

---
+++ InnoMetricsCollector/InnoMetricsCollector/ActiveProcess+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  ActiveProcess+CoreDataClass.swift
+//  InnoMetricsCollector
+//
+//  Created by Dragos Strugar on 13.03.2020.
+//  Copyright © 2020 Innopolis University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class ActiveProcess: NSManagedObject {
+}
---
+++ InnoMetricsCollector/InnoMetricsCollector/ActiveProcess+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveProcess+CoreDataProperties.swift
+//  InnoMetricsCollector
+//
+//  Created by Dragos Strugar on 13.03.2020.
+//  Copyright © 2020 Innopolis University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension ActiveProcess {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ActiveProcess> {
+        return NSFetchRequest<ActiveProcess>(entityName: "ActiveProcess")
+    }
+    
+    @NSManaged public var process_name: String!
+    @NSManaged public var measurementReportList: Set<EnergyMeasurement>?
+    @NSManaged public var session: Session?
+}
